76ad47d4f83589948bf13cb3f984a318fe21f5c8
oumaymabg <oumaymabou257@gmail.com>
2021-12-05 23:55:50 +0100

    app

---
+++ resources/views/inscription.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <link rel="stylesheet" type="text/css" href="./css/signup.css">
+</head>
+<body>
+<form> 
+        <h1>SignUp</h1>
+        <input type="text" name="" placeholder="Username">
+        <input type="text" name="" list="tech">
+        <datalist id="tech">
+            <option>HTML</option>
+            <option>CSS</option>
+            <option>JavaScript</option>
+        </datalist>
+<input type="email" name="" placeholder="Email">
+<input type="password" name="" placeholder="Password">
+<input type="password" name="" placeholder="Confirm Password">
+<button type="Submite">SignUp</button>
+</form>
+</body>
+</html>
